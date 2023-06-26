1a25200da86e26ec4e785b14407e2613ac9a55ac
Facu185 <facufa12@gmail.com>
2023-06-25 23:28:42 -0300

    refactor

--- src/pages/login.php
+++ src/pages/login.php
@@ -1,6 +1,5 @@
 <?php 
     session_start();
-    //#48BBED
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,16 +16,17 @@
             <h2 class="menu__welcome">Bienvenido</h2>
             <p class="menu__text">Ingrese para poder acceder a mas funcionalidades</p>
             <div class="login__form">
-                <form action="submit" method="get">
+                <form action="submit" method="post">
                     <input type="email" id="loginEmail" name="email" placeholder="Email">
                     <input type="password" id="loginPassword" name="password" placeholder="Contraseña">
-                    <button type="submit" class="button--primary">Ingresar</button>
+                    <button type="button" class="button--primary" id="loginButton">Ingresar</button>
                 </form>
-                <p class="login__register">No tienes una cuenta? Registrate <span>aqui.</span> </p>
+                <p class="login__register">No tienes una cuenta? Registrate <a href="./pages/register.php"><span>aqui.</span></a> </p>
             </div>
             <p class="login__conditions">Al hacer click en ingresar estas de acuerdo con los <span>terminos y condiciones.</span> </p>
 
         </section>
     </main>
+    <script src="./js/index.js" type="module"></script>
 </body>
 </html>
