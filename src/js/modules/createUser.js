f98c64dcd2e98cd5c17f3750143e8baf0cbfffb1
Facu185 <facufa12@gmail.com>
2023-07-08 22:48:14 -0300

    refactor added try catch to improve error control

--- src/js/modules/createUser.js
+++ src/js/modules/createUser.js
@@ -1,50 +1,53 @@
 const register = () => {
-  const name = document.getElementById("registerName").value;
-  const lastName = document.getElementById("registerLastName").value;
-  const phoneNumber = document.getElementById("registerPhone").value;
-  const email = document.getElementById("registerEmail").value;
-  const password = document.getElementById("registerPassword").value;
+  try {
+    const name = document.getElementById("registerName").value;
+    const lastName = document.getElementById("registerLastName").value;
+    const phoneNumber = document.getElementById("registerPhone").value;
+    const email = document.getElementById("registerEmail").value;
+    const password = document.getElementById("registerPassword").value;
 
-  if (!name || !lastName || !phoneNumber || !email || !password) {
-    console.log("falta completar campos");
-    return;
+    function contieneNumeros(string) {
+      var regex = /\d/;
+      return regex.test(string);
+    }
+
+    if (contieneNumeros(name) || contieneNumeros(lastName))
+      throw new Error("El nombre y el apellido no pueden contener numeros");
+
+    if (!name || !lastName || !phoneNumber || !email || !password)
+      throw new Error("falta completar campos");
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regexEmail.test(email))
+      throw new Error("Direccion de correo electronico no valida");
+    if (password.length < 6)
+      throw new Error("La contraseña no puede ser menor a 6 digitos");
+    if (phoneNumber.length !== 9)
+      throw new Error("El numero de telefono debe ser de 9 digitos");
+    let existEmail = data.filter((e) => e.email === email);
+    if (existEmail.length > 0)
+      throw new Error("Ya existe un usuario con este email");
+
+    function Users({ email, password, name, lastName, phoneNumber }) {
+      this.email = email;
+      this.password = password;
+      this.name = name;
+      this.lastName = lastName;
+      this.phoneNumber = phoneNumber;
+    }
+    let user = new Users({
+      email,
+      password,
+      name,
+      lastName,
+      phoneNumber,
+    });
+    data.push(user);
+    localStorage.setItem("login", JSON.stringify(data));
+    console.log("La cuenta fue creada con exito");
+    return user;
+  } catch (error) {
+    console.error(error);
   }
-  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!regexEmail.test(email)) {
-    console.log("Dirección de correo electrónico invalida");
-    return;
-  }
-  if(password.length < 6){
-    console.log("La contraseña es muy corta");
-    return;
-  }
-  if(phoneNumber.length !== 9){
-    console.log("El telefono debe ser de 9 caracteres");
-    return;
-  }
-  let existEmail = data.filter(e => e.email === email);
-  if(existEmail.length > 0){
-    console.log("Este email ya esta registrado.")
-    return;
-  }
-  function Users({ email, password, name, lastName, phoneNumber }) {
-    this.email = email;
-    this.password = password;
-    this.name = name;
-    this.lastName = lastName;
-    this.phoneNumber = phoneNumber;
-  }
-  let user = new Users({
-    email,
-    password,
-    name,
-    lastName,
-    phoneNumber,
-  });
-  data.push(user);
-  localStorage.setItem("login", JSON.stringify(data));
-  console.log("La cuenta fue creada con exito");
-  return user;
 };
 const registerButton = document.getElementById("registerButton");
 if (!localStorage.getItem("login")) {
