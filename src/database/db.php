06077530dc49100e88c71851839dbe7afd1dff64
Facu185 <facufa12@gmail.com>
2023-11-12 17:59:55 -0300

    .

--- src/database/db.php
+++ src/database/db.php
@@ -1,15 +1,21 @@
-<?php 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "pe3_urunet";
+<?php
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
+/* require_once './vendor/autoload.php';
 
-    try 
-    {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) 
-    {
-        echo "Error de conexión: " . $e->getMessage();
-    }
+$dotenv = Dotenv\Dotenv::createImmutable("./");
+$dotenv->load();
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME']; */
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
 ?>
